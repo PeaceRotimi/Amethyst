9d46d7fd66f8c935ab567e5ec19662ae374f8a15
Trigzy <trish.sif@gmail.com>
2025-03-08 16:48:59 +0000

    django stuff

---
+++ back/house/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+from django.contrib import admin
+from django.urls import path, include
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('house/', include('house.urls')),
+]
